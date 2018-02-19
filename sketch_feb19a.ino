dd17b8c6931d40baca263649a82c04f03089c739
HishamEsaam <36447324+HishamEsaam@users.noreply.github.com>
2018-02-19 21:28:36 +0200

    Add files via upload

---
+++ sketch_feb19a.ino
@@ -0,0 +1,19 @@
+int led=5;
+int sensor=A0;
+int sensorvalue;
+
+void setup() 
+{
+  pinMode(led,OUTPUT);
+
+}
+
+void loop()
+{
+  sensorvalue=analogRead(sensor);
+  digitalWrite(led,HIGH);
+  delay(sensorvalue);
+  digitalWrite(led,LOW);
+  delay(sensorvalue);
+
+}
